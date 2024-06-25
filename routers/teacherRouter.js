0c120cdbde37f72d5b1ff59f8e375ba221289292
rakshyak-98 <rakshak1998@gmail.com>
2024-06-25 22:05:30 +0530

    add access constraint for get teacher

--- routers/teacherRouter.js
+++ routers/teacherRouter.js
@@ -3,7 +3,7 @@ const { ensureAccess } = require("../middlewares/auth");
 const router = require("express").Router();
 
 router.post("/", ensureAccess(["Admin"]), controller.addTeacher);
-router.get("/", controller.getTeacherList);
+router.get("/", ensureAccess(["Admin"]), controller.getTeacherList);
 
 module.exports = router;
 
