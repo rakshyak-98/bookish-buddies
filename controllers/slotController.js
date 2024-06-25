a4ea32b7576e6b1adecbb5ec9c9b5721388280b0
rakshyak-98 <rakshak1998@gmail.com>
2024-06-25 23:17:42 +0530

    implement validation function for slot query

--- controllers/slotController.js
+++ controllers/slotController.js
@@ -1,7 +1,7 @@
-const { validateSlot } = require("../lib/validateDate");
+const { validateSlot, validateSlotQuery } = require("../lib/validateDate");
 const Slot = require("../models/Slot");
 
-function addSlot(req, res) {
+async function addSlot(req, res) {
 	const validSlot = validateSlot(req.body);
 	if (validSlot.error) {
 		res.status(400).send(validSlot.error);
@@ -9,11 +9,27 @@ function addSlot(req, res) {
 	}
 
 	const slot = new Slot(validSlot.data);
-	slot.save();
+	await slot.save();
 	res.status(201).send({ _id: slot._id });
+}
+
+async function getSlotList(req, res) {
+	if (Object.keys(req.query) !== 0) {
+		const validSlot = validateSlotQuery(req.query);
+		if (validSlot.error) {
+			res.status(400).send(validSlot.error);
+			return;
+		}
+		const slots = await Slot.find(validSlot.data);
+		res.status(200).send(slots);
+	} else {
+		const slots = await Slot.find();
+		res.status(200).send(slots);
+	}
 }
 
 module.exports = {
 	addSlot,
+	getSlotList,
 };
 
--- routers/slotRouter.js
+++ routers/slotRouter.js
@@ -3,6 +3,7 @@ const { ensureAccess } = require("../middlewares/auth");
 const router = require("express").Router();
 
 router.post("/", ensureAccess(["teacher"]), controller.addSlot);
+router.get("/", controller.getSlotList);
 
 module.exports = router;
 
